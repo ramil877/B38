fe13220e266c942b8c9c2e75eb7f277a7e3b93f2
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-04-12 14:07:31 +0300

    Задание 2

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -2,5 +2,9 @@ public class FirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    Point p1 = new Point(7,4);
+    Point p2 = new Point(5,2);
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
   }
 }
