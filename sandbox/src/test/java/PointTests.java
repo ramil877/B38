6d843b41ad768877a6d72052deb90c95d153bb55
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-04-13 17:49:40 +0300

    Выполнено задание 3

--- sandbox/src/test/java/PointTests.java
+++ sandbox/src/test/java/PointTests.java
@@ -1,4 +1,30 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PointTests {
 
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(7,4);
+        Point p2 = new Point(5,2);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+    }
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(2,3);
+        Point p2 = new Point(3,5);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+    }
+    @Test
+    public void testDistance3() {
+        Point p1 = new Point(4,5);
+        Point p2 = new Point(10,11);
+        Assert.assertEquals(p1.distance(p2), 8.48528137423857);
+    }
+    @Test
+    public void testDistance4() {
+        Point p1 = new Point(125,75);
+        Point p2 = new Point(222,99);
+        Assert.assertEquals(p1.distance(p2), 88.48528137423857);
+    }
 }
