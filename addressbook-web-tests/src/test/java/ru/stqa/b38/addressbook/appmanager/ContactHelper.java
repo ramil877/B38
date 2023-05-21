d962f254ce58f3d823fe73019c836fa6e7c1a48d
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-05-21 23:26:54 +0300

    Кеширование

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/ContactHelper.java
@@ -58,12 +58,14 @@ public class ContactHelper extends BaseHelper {
         initContactCreation();
         fillForm(contact);
         submitContactCreation();
+        contactCache = null;
         returnToHomePage();
     }
 
     public void delete(ContactData contact) {
         selectContactById(contact.getId());
         deleteContact();
+        contactCache = null;
         waitAlertAccept(Duration.ofSeconds(2));
         wd.switchTo().alert().accept();
         try {
@@ -77,6 +79,7 @@ public class ContactHelper extends BaseHelper {
         initModification();
         fillForm(contact);
         submitModification();
+        contactCache = null;
         returnToHomePage();
     }
 
@@ -88,16 +91,20 @@ public class ContactHelper extends BaseHelper {
         return wd.findElements(By.name("selected[]")).size();
     }
 
+    private Contacts contactCache = null;
     public Contacts all() {
-        Contacts contacts = new Contacts();
+        if (contactCache != null) {
+            return new Contacts(contactCache);
+        }
+        contactCache = new Contacts();
         List<WebElement> elements = wd.findElements(By.xpath("//table[@id='maintable']//*[@name='entry']"));
         for (WebElement element : elements) {
             String firstname = element.findElement(By.xpath(".//td[3]")).getText();
             String lastname = element.findElement(By.xpath(".//td[2]")).getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+            contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
         }
-        return contacts;
+        return new Contacts(contactCache);
     }
 
     public void waitAlertAccept(Duration timeOutInSeconds) {
--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/GroupHelper.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.b38.addressbook.model.GroupData;
 import ru.stqa.b38.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends BaseHelper {
     public GroupHelper(WebDriver wd) {
@@ -53,6 +51,7 @@ public class GroupHelper extends BaseHelper {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache = null;
         returnToGroupPage();
     }
 
@@ -61,12 +60,14 @@ public class GroupHelper extends BaseHelper {
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
+        groupCache = null;
         returnToGroupPage();
     }
 
     public void delete(GroupData group) {
         selectGroupById(group.getId());
         deleteGroup();
+        groupCache = null;
         returnToGroupPage();
     }
 
@@ -74,16 +75,19 @@ public class GroupHelper extends BaseHelper {
         return isElementPresent(By.name("selected[]"));
     }
 
-
+    private Groups groupCache = null;
     public Groups all() {
-        Groups groups = new Groups();
+        if (groupCache != null) {
+            return new Groups(groupCache);
+        }
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
     }
 
 
