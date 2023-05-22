f4d7a2d92b38124133c8e236a87a8c27dbb95890
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-05-23 00:01:17 +0300

    Лекция 5.11 Тест для проверки телефонов, метод обратных проверок

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/model/ContactData.java
@@ -4,115 +4,130 @@ import java.util.Objects;
 
 public class ContactData {
 
-  private int id = Integer.MAX_VALUE;;
-  private  String firstname;
-  private String lastname;
-  private String address;
-  private String email;
-  private String mobile;
-  private String homePhone;
+    private int id = Integer.MAX_VALUE;
+    ;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String email;
+    private String mobile;
+    private String homePhone;
 
-  private String mobilePhone;
-  private String workPhone;
+    private String mobilePhone;
+    private String workPhone;
+    private String allPhones;
 
-  public String getHomePhone() {
-    return homePhone;
-  }
+    public String getAllPhones() {
+        return allPhones;
+    }
 
-  public String getMobilePhone() {
-    return mobilePhone;
-  }
+    public ContactData withAllPhones(String allPhones) {
+        this.allPhones = allPhones;
+        return this;
+    }
 
-  public String getWorkPhone() {
-    return workPhone;
-  }
+    public String getHomePhone() {
+        return homePhone;
+    }
 
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
 
-  public int getId() {
-    return id;
-  }
-  public String getFirstname() {
-    return firstname;
-  }
-
-  public String getLastname() {
-    return lastname;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public String getMobile() {
-    return mobile;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public ContactData withId(int id) {
-    this.id = id;
-    return this;
-  }
-
-  public ContactData withFirstname(String firstname) {
-    this.firstname = firstname;
-    return this;
-  }
-
-  public ContactData withLastname(String lastname) {
-    this.lastname = lastname;
-    return this;
-  }
-
-  public ContactData withAddress(String address) {
-    this.address = address;
-    return this;
-  }
-
-  public ContactData withMobile(String mobile) {
-    this.mobile = mobile;
-    return this;
-  }
-
-  public ContactData withEmail(String email) {
-    this.email = email;
-    return this;
-  }
-  public ContactData withHomePhone(String homePhone) {
-    this.homePhone = homePhone;
-    return this;
-  }
-  public ContactData withMobilePhone(String mobilePhone) {
-    this.mobilePhone = mobilePhone;
-    return this;
-  }
-  public ContactData withWorkPhone(String workPhone) {
-    this.workPhone = workPhone;
-    return this;
-  }
+    public String getWorkPhone() {
+        return workPhone;
+    }
 
 
-  @Override
-  public String toString() {
-    return "ContactData{" +
-            "id=" + id +
-            ", firstname='" + firstname + '\'' +
-            ", lastname='" + lastname + '\'' +
-            '}';
-  }
+    public int getId() {
+        return id;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ContactData that = (ContactData) o;
-    return id == that.id && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
-  }
+    public String getFirstname() {
+        return firstname;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, firstname, lastname);
-  }
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ContactData withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public ContactData withFirstname(String firstname) {
+        this.firstname = firstname;
+        return this;
+    }
+
+    public ContactData withLastname(String lastname) {
+        this.lastname = lastname;
+        return this;
+    }
+
+    public ContactData withAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public ContactData withMobile(String mobile) {
+        this.mobile = mobile;
+        return this;
+    }
+
+    public ContactData withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public ContactData withHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+        return this;
+    }
+
+    public ContactData withMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+        return this;
+    }
+
+    public ContactData withWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return id == that.id && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname);
+    }
 }
