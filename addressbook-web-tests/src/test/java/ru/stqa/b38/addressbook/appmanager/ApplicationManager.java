7277451e4d583945eb4d39e985a5e9f69f7a9a28
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-05-29 23:37:59 +0300

    Revert "Описание конфигурации"
    This reverts commit 69868d35994553ed30c93babf59ad497ff982857.

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/appmanager/ApplicationManager.java
@@ -4,44 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private final String browser;
-  private final Properties properties;
+  private final Browser browser;
   WebDriver wd;
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(Browser browser) {
     this.browser = browser;
-    properties = new Properties();
   }
 
-  public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/tests/resources/%s.properties", target))));
-    if (browser.equals("chrome")) {
+  public void init() {
+    if (browser.equals(Browser.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser.equals("firefox")) {
+    } else if (browser.equals(Browser.FIREFOX)) {
       wd = new FirefoxDriver();
     }
 
     System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    sessionHelper.login("admin", "secret");
   }
 
   public void getLogout() {
