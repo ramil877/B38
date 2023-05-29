b64e2d095fce111072a4d2d171946c3b23735144
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-05-29 23:32:27 +0300

    Revert "отмена действий"
    This reverts commit aa1dfbec533cd1faa1d58557b995b55018b903df.

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.b38.addressbook.tests;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.b38.addressbook.appmanager.ApplicationManager;
 
@@ -13,7 +14,7 @@ public class TestBase {
         app.init();
     }
 
-    @BeforeMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
