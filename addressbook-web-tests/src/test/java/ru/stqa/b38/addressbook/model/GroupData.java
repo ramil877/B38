457423fde7da36c63805879a513f862960424101
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-05-31 23:29:58 +0300

    Создант тест HbConnection для групп

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/model/GroupData.java
@@ -1,18 +1,33 @@
 package ru.stqa.b38.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
+@Entity
+@Table(name = "group_list")
 public class GroupData {
+  @Id
+  @Column(name = "group_id")
 
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "group_name")
   private String name;
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text")
   private String header;
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
   private String footer;
+
 
   @Override
   public boolean equals(Object o) {
