f5af34aed6f085e45d7b62f444de7a78fad4f187
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-06-04 00:46:02 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/b38/addressbook/tests/GroupCreationTests.java
@@ -60,5 +60,6 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        verifyGroupListInUI();
     }
 }
